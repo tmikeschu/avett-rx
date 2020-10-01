82f39c9afd88f8c7a366c608349cae0bf19915d6
Mike Schutte <tmikeschutte@gmail.com>
2020-09-30 21:03:19 -0400

    fetch tags on homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import "../styles/globals.css";
 
 import * as React from "react";
+import { ApolloProvider } from "@apollo/client";
 import { AppProps } from "next/app";
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import { useApollo } from "lib/apollo-client";
+import { Any } from "lib/types";
+
+const MyApp: React.FC<AppProps<{
+  initialApolloState: Record<string, Any>;
+}>> = ({ Component, pageProps }) => {
+  const client = useApollo(pageProps.initialApolloState);
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 };
 
 export default MyApp;
