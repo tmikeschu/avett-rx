c8f5e13a311f78f53d0690e68233847f7d7f5094
Mike Schutte <tmikeschutte@gmail.com>
2020-10-13 17:04:37 -0400

    clarify auth0 user shape

--- src/lib/use-current-user.ts
+++ src/lib/use-current-user.ts
@@ -5,7 +5,7 @@ import { AuthedUser } from "lib/types";
 const FAUNA_SECRET_KEY = "https://faunadb.com/id/secret";
 
 export default function useCurrentUser(): { user?: AuthedUser } {
-  const { user } = useAuth0() as { user: AuthedUser };
+  const { user } = useAuth0() as { user: Omit<AuthedUser, "token"> };
   return {
     user: user ? { ...user, token: user[FAUNA_SECRET_KEY] } : undefined,
   };
