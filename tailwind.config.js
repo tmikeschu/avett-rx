d9f83535ceac30cf46d7d96b8d7cf60a1a0b5ce6
Mike Schutte <tmikeschutte@gmail.com>
2020-10-14 16:47:07 -0400

    extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,33 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Arapey", "serif"],
+      body: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        /**
+         * primary and secondary: https://www.colourlovers.com/palette/4667031/Kannamma_Maurie
+         * success, error, warning: https://www.colourlovers.com/palette/629637/(%E2%97%95%E3%80%9D%E2%97%95)
+         */
+        primary: {
+          default: "#131665",
+        },
+        secondary: {
+          default: "BDB5F5",
+        },
+        success: {
+          default: "#83AF9B",
+        },
+        error: {
+          default: "#FE4365",
+        },
+        warning: {
+          default: "#F9CDAD",
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
