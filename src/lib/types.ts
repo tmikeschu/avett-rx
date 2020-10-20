2f736d7eacf649f0da6869b68edc346e47a0e851
Mike Schutte <tmikeschutte@gmail.com>
2020-10-20 12:38:30 -0400

    simplify codegen config

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,6 @@
 import { User } from "api";
 
-//eslint-disable-next-line
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Any = any;
 
 export interface AuthedUser extends User {
