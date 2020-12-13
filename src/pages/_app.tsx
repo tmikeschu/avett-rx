325e5bc167e21ff7cf756e6d63b160a50d81a6cb
Mike Schutte <tmikeschutte@gmail.com>
2020-12-13 11:27:38 -0500

    meta data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { theme } from "styles/theme";
 
 import { ApolloProvider } from "lib/apollo-client";
@@ -21,6 +22,33 @@ const AvettRxApp: React.FC<AppProps> = ({
     <ChakraProvider theme={theme}>
       <AuthProvider>
         <ApolloProvider initialState={initialApolloState}>
+          <Head>
+            <title>Avett Rx</title>
+            <meta name="title" content="Avett Rx" />
+            <meta
+              name="description"
+              content="You've got the feels. They've got the tunes. Get an Avett Brothers song prescribed for whatever mood you'd like."
+            />
+            <meta name="image" content="/social.png" />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://avettrx.com/" />
+            <meta property="og:title" content="Avett Rx" />
+            <meta
+              property="og:description"
+              content="You've got the feels. They've got the tunes. Get an Avett Brothers song prescribed for whatever mood you'd like."
+            />
+            <meta property="og:image" content="/social.png" />
+
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://avettrx.com/" />
+            <meta property="twitter:title" content="Avett Rx" />
+            <meta
+              property="twitter:description"
+              content="You've got the feels.  They've got the tunes. Get an Avett Brothers song prescribed for whatever mood you'd like."
+            />
+            <meta property="twitter:image" content="/social.png"></meta>
+          </Head>
           <Component {...pageProps} />
         </ApolloProvider>
       </AuthProvider>
