2b8d7ed2da19007266bb5f6ae818df21cac783df
Mike Schutte <tmikeschutte@gmail.com>
2020-12-12 21:54:35 -0500

    update _app pageProps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { theme } from "styles/theme";
 
-import Layout from "components/layout";
-import { ApolloProvider, InitialState } from "lib/apollo-client";
+import { ApolloProvider } from "lib/apollo-client";
 import { AuthProvider } from "lib/auth";
 import mockServiceWorker from "mocks";
 
@@ -14,16 +13,15 @@ if (process.env.MOCK_SERVICE_WORKER === "1") {
   mockServiceWorker();
 }
 
-const AvettRxApp: React.FC<AppProps<{
-  initialApolloState: InitialState;
-}>> = ({ Component, pageProps }) => {
+const AvettRxApp: React.FC<AppProps> = ({
+  Component,
+  pageProps: { initialApolloState = {}, ...pageProps },
+}) => {
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
-        <ApolloProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+        <ApolloProvider initialState={initialApolloState}>
+          <Component {...pageProps} />
         </ApolloProvider>
       </AuthProvider>
     </ChakraProvider>
