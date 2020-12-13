33ce0c89ce1a32e626b06b6d08eff1a8758d1e68
Mike Schutte <tmikeschutte@gmail.com>
2020-12-13 11:43:06 -0500

    image uri

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ if (process.env.MOCK_SERVICE_WORKER === "1") {
   mockServiceWorker();
 }
 
+const IMAGE_URI = "https://avettrx.com/social.png";
 const AvettRxApp: React.FC<AppProps> = ({
   Component,
   pageProps: { initialApolloState = {}, ...pageProps },
@@ -29,7 +30,7 @@ const AvettRxApp: React.FC<AppProps> = ({
               name="description"
               content="You've got the feels. They've got the tunes. Get an Avett Brothers song prescribed for whatever mood you'd like."
             />
-            <meta name="image" content="/social.png" />
+            <meta name="image" content={IMAGE_URI} />
 
             <meta property="og:type" content="website" />
             <meta property="og:url" content="https://avettrx.com/" />
@@ -38,7 +39,7 @@ const AvettRxApp: React.FC<AppProps> = ({
               property="og:description"
               content="You've got the feels. They've got the tunes. Get an Avett Brothers song prescribed for whatever mood you'd like."
             />
-            <meta property="og:image" content="/social.png" />
+            <meta property="og:image" content={IMAGE_URI} />
 
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content="https://avettrx.com/" />
@@ -47,7 +48,7 @@ const AvettRxApp: React.FC<AppProps> = ({
               property="twitter:description"
               content="You've got the feels.  They've got the tunes. Get an Avett Brothers song prescribed for whatever mood you'd like."
             />
-            <meta property="twitter:image" content="/social.png"></meta>
+            <meta property="twitter:image" content={IMAGE_URI}></meta>
           </Head>
           <Component {...pageProps} />
         </ApolloProvider>
