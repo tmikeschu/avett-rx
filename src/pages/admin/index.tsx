a4f6ee899aedc40e3cc45391d50dc9343eef7ee8
Mike Schutte <tmikeschutte@gmail.com>
2020-12-19 11:17:44 -0500

    refine admin home page

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,23 +1,31 @@
 import * as React from "react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import { NextPage } from "next";
-import { useRouter } from "next/router";
 
-import { Role, User } from "api";
 import { LoginButton, LogoutButton } from "features/auth";
 import { useAuthContext } from "lib/auth";
-import { Route } from "lib/routes";
-
-const isAdmin = (u: User): boolean => u.roles.includes(Role.Admin);
 
 const AdminHome: NextPage = () => {
-  const { push } = useRouter();
   const { user } = useAuthContext();
-  React.useEffect(() => {
-    if (user && !isAdmin(user)) {
-      push(Route.Root);
-    }
-  }, [user, push]);
-  return user ? <LogoutButton /> : <LoginButton />;
+  return user ? (
+    <Flex direction="column" align="flex-start" w="full" h="full" pt="4">
+      <Flex w="full" justify="space-between" px={[4, null, 0]}>
+        <Box>
+          <Heading as="h1" fontSize="2xl">
+            Avett Rx Admin
+          </Heading>
+          <Text fontSize="sm" color="gray.600">
+            {user.email}
+          </Text>
+        </Box>
+        <LogoutButton />
+      </Flex>
+    </Flex>
+  ) : (
+    <Flex align="center" justify="center" direction="column" h="full" w="full">
+      <LoginButton />
+    </Flex>
+  );
 };
 
 export default AdminHome;
